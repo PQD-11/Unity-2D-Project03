4c5f5d66d6da929bd9a80c5e8c1477c78306e488
Dang Pham <85118356+PQD-11@users.noreply.github.com>
2023-09-21 00:52:12 +0700

    [handle] PlantEnemy

---
+++ Project03_2DPlatformer/Assets/_Scripts/Enemies/AIStaticEnemyBrain.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SVS.AI
+{
+    public class AIStaticEnemyBrain : AIEnemy
+    {
+        public AIBehavior AttackBehavior;
+
+        private void Update()
+        {
+            // AttackBehavior.PerformAction(this);
+        }
+    }
+}
+
